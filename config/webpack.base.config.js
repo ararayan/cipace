83b6171718889550ff7fdb68be9e8aca4294583e
ararayan <ararayan77@gmail.com>
2017-08-08 19:14:36 +0800

    use ts-node for webpack and node es 6 style

--- config/webpack.base.config.js
+++
@@ -1,5 +0,0 @@
-var baseConfig = require('./webpack.base.config')
-
-module.exports = {
-
-}
---
+++ config/webpack.base.config.ts
@@ -0,0 +1,7 @@
+import { access } from 'fs';
+import * as webpack from 'webpack';
+
+const config: webpack.Module = {
+a: 2,
+  b: 3,
+};
